9ff8a4ee2e4eff0503881e0f8d0e37ff58f790fd
GautamaBTC <42926100+GautamaBTC@users.noreply.github.com>
2025-05-02 01:46:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/vipauto/',  // <- имя вашего репозитория с косой чертой
+  base: '/vipauto/',
   plugins: [vue()]
 })
